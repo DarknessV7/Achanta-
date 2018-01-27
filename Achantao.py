dd7c93d6af01834646d8f06d3d6ae1290b80d92f
DarknessV7 <diegixfezent789@gmail.com>
2018-01-27 14:34:36 -0600

    Funciones

---
+++ Achantao.py
@@ -0,0 +1,14 @@
+def apellido (): 
+	apellido= "Felipe"
+	print apellido 
+	edad ()	
+
+def edad ():
+	edad=15 
+	print edad
+ 
+apellido ()
+
+
+
+
---
+++ Achantaoo.py
@@ -0,0 +1,7 @@
+def validar():
+	if 3>5 and 8>3:
+		print "T"
+	else: 
+		print "F"	
+
+validar ()
